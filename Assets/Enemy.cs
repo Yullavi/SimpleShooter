dcd3db2a872ad0e4f7725d93b2066ac15300a690
Yullavi <yulla@inbox.ru>
2016-09-21 22:41:10 +0300

    Game for practice

---
+++ Assets/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private GameObject man;
+	// Use this for initialization
+	void Start () {
+        man = GameObject.Find("Man");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, man.transform.position, step);
+    }
+}
---
+++ Assets/Terrain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terrain : MonoBehaviour
+{
+    public float speed;
+    public Camera cam;
+    public GameObject man;
+    private Vector3 target;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.point;
+            }
+        }
+        float step = speed * Time.deltaTime;
+        man.transform.position = Vector3.MoveTowards(man.transform.position, target, step);
+    }
+}
